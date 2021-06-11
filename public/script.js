501574130057ff955ca47d5462ede90c4167b321
apinkpanda722 <iih907@naver.com>
2021-06-11 11:32:13 +0900

    이거로

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer();
 const myVideo = document.createElement("video");
-myVideo.muted = false;
+myVideo.muted = true;
 
 navigator.mediaDevices
   .getUserMedia({
